d2a42005d932354562b713a3e1135eba029ce663
sarrajaballah <sarrajaballah.s@gmail.com>
2020-10-09 16:41:15 +0200

    get /id initialized

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-
+app.get('/id',(req,res)=>{
+    res.send({"id":"it340-tanzia_brawlers"});
+});
 app.listen(3000, () => {
     console.log("Server started !");
 });
