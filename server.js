4fc3d5dd8b011077d5185bc02b0171ccbb2dd36d
unknown <amenallahslama@gmail.com>
2020-11-26 11:37:21 +0100

    add standard js closes #3

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
-const express = require("express");
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/id',(req,res)=>{
-    res.send({"id":"it340-tanzia_brawlers"});
-});
+app.get('/id', (req, res) => {
+  res.send({ id: 'it340-tanzia_brawlers' })
+})
 
 function sum (price, quantity) {
-    let sum = 0;
-    price.forEach((element,index) => sum+= element*quantity[index]);
-    return sum;
+  let sum = 0
+  price.forEach((element, index) => { sum += element * quantity[index] })
+  return sum
 }
 
-app.post('/bill', (req,res) => {
-    let {prices : price, quantities : quantity} = req.body;
-    if (price == undefined || quantity == undefined || price.length != quantity.length) 
-        res.send({ "error" : "error message" });
-    else    
-        res.send({"total": sum(price,quantity)});
-});
+app.post('/bill', (req, res) => {
+  const { prices: price, quantities: quantity } = req.body
+  if (price === undefined || quantity === undefined || price.length !== quantity.length) { res.send({ error: 'error message' }) } else { res.send({ total: sum(price, quantity) }) }
+})
 
 app.listen(3000, () => {
-    console.log("Server started !");
-});
+  console.log('Server started !')
+})
