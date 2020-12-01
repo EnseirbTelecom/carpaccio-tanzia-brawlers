02da3a90a01b381ffaab5dd26a4764125df0b1d8
unknown <amenallahslama@gmail.com>
2020-12-01 14:23:06 +0100

    debut modulariisation

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ app.get('/id', (req, res) => {
   res.send({ id: 'it340-tanzia_brawlers' })
 })
 
-function sum (price, quantity) {
-  let sum = 0
-  price.forEach((element, index) => { sum += element * quantity[index] })
-  return sum
-}
-
 app.post('/bill', (req, res) => {
   const { prices: price, quantities: quantity } = req.body
   if (price === undefined || quantity === undefined || price.length !== quantity.length) { res.send({ error: 'error message' }) } else { res.send({ total: sum(price, quantity) }) }
